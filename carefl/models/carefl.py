0fbc989434f68d949fdcdaa5c50b8ffca8bb0da4
hashikami <hashikun917@icloud.com>
2025-07-29 19:44:38 +0900

    careflのflow逆トポロジカル順&介入分布からの生成モジュールの修正

--- carefl/models/carefl.py
+++ carefl/models/carefl.py
@@ -13,7 +13,7 @@ from carefl.nflib.nets import MLP4
 from carefl.data.generate_synth_data import CustomSyntheticDataset
 
 
-class CareFLTrainingPipeline:
+class CAREFL:
     def __init__(self, config):
         self.config = config
         self.meta_data = self.config.meta_data
@@ -21,9 +21,11 @@ class CareFLTrainingPipeline:
         self.carefl = self.config.carefl
         self.training = self.config.training
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        
+        self.flow = None
 
 
-    def simulate_sem(self):
+    def _simulate_sem(self):
         
         d = self.meta_data.d
         n = self.meta_data.n_samples
@@ -60,6 +62,33 @@ class CareFLTrainingPipeline:
         if not is_dag(B_est):
             raise ValueError('B_est should be a DAG')
         return B_est
+    
+    def predict_intervention(self, int_idx, int_val, n_samples=100):
+    
+        if isinstance(int_idx, (int, np.integer)):
+            int_idx = [int_idx]
+        if np.isscalar(int_val):
+            int_val = [float(int_val)]
+        assert len(int_idx) == len(int_val)
+
+        int_idx = list(int_idx)
+        int_val = list(int_val)
+        device = self.device
+        
+        flows = self.flow.flow.flows
+        z = self.flow.prior.sample((n_samples,)).to(device)
+        for affine in flows[::-1]:
+            trans_idx = affine.trans_idx[0]
+            if trans_idx in int_idx:
+                z[:, trans_idx] = int_val[int_idx.index(trans_idx)] # 介入値を強制的に設定
+            else:
+                z, _ = affine.backward(z)
+                
+        return z
+    
+        
+    def predict_counterfactual(self, ):
+        return 
 
 
     def _get_flow_arch(self):
@@ -84,7 +113,8 @@ class CareFLTrainingPipeline:
         
         ordered_vertices = G.topological_sorting()
         flow_list = []
-        for v in ordered_vertices:
+        for v in ordered_vertices[::-1]:
+        #for v in ordered_vertices:
             cond_idx = G.neighbors(v, mode=ig.IN)
             affine = DAGAffineCL(d, cond_idx, [v], net_class, self.carefl.nh, self.carefl.scale_shift_base)
             flow_list.append(affine)
@@ -97,7 +127,7 @@ class CareFLTrainingPipeline:
 
     def _train(self):
         
-        self.X = self.simulate_sem()
+        self.X = self._simulate_sem()
         dset = self._get_datasets(self.X)
         train_loader = DataLoader(dset, shuffle=True, batch_size=self.training.batch_size)
 
@@ -121,5 +151,16 @@ class CareFLTrainingPipeline:
                 
             loss_vals.append(loss_val) # バッチごとに割らないでよいのか？？？
             
+        self.flow = flow
         return flow, loss_vals
         
+        
+    def _forward_flow(self, data):
+        if self.flow is None:
+            raise ValueError('Model needs to be fitted first')
+        return self.flow.forward(torch.tensor(data.astype(np.float32)).to(self.device))[0][-1].detach().cpu().numpy()
+
+    def _backward_flow(self, latent):
+        if self.flow is None:
+            raise ValueError('Model needs to be fitted first')
+        return self.flow.backward(torch.tensor(latent.astype(np.float32)).to(self.device))[0][-1].detach().cpu().numpy()
